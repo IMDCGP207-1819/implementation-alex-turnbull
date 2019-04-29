e9e68780aff15a2f15ec4f4ecae6d3680e939757
alex-turnbull <alexturnbull1@virginmedia.com>
2019-04-29 22:48:27 +0100

    moved function definitions from .cpp to .h for Window class

--- s192255-GameEngine/s192255-GameEngine/Window.h
+++ s192255-GameEngine/s192255-GameEngine/Window.h
@@ -9,9 +9,18 @@ public:
 	Window();
 	~Window();
 
-	void CreateWindow(int width, int height);
+	void CreateWindow(int width, int height)
+	{
+		GameWindow = new sf::RenderWindow(sf::VideoMode(width, height), "Game Window");
+		GameWindow->setPosition(sf::Vector2i((1920 / 2) - (width / 2), (1080 / 2) - (height / 2)));
+	}
 
-	void CreateWindow(int width, int height, int frameRateLimit);
+	void CreateWindow(int width, int height, int frameRateLimit)
+	{
+		GameWindow = new sf::RenderWindow(sf::VideoMode(width, height), "Game Window");
+		GameWindow->setPosition(sf::Vector2i((1920 / 2) - (width / 2), (1080 / 2) - (height / 2)));
+		GameWindow->setFramerateLimit(frameRateLimit);
+	}
 
 	static Window* _instance;
 
